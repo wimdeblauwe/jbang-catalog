d4dedd60eb77c49818cb898e7570a9c261fdd482
jreleaserbot <jreleaser@kordamp.org>
2026-06-05 08:36:48 +0000

    Bump ttcli to 1.12.1.

---
+++ ttcli.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 25+
+//DEPS io.github.wimdeblauwe:ttcli-single-jar:1.12.1
+// Generated with JReleaser 1.24.0 at 2026-06-05T08:36:24.61250116Z
+
+public class ttcli {
+    public static void main(String... args) throws Exception {
+        io.github.wimdeblauwe.ttcli.TamingThymeleafCliApplication.main(args);
+    }
+}
